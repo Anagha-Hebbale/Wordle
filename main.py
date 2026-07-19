b071d39ab5c641119fdad8df5d2197abd02f83b5
Anagha-Hebbale <hebbaleanagha@gmail.com>
2026-07-19 14:06:31 +0530

    added no. of tries

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ print("Welcome to Wordle!")
 
 secret="apple"
 print("Guess the 5-Letter Word:")
-guess=input()
-code=[]
-i=0
-if guess==secret:
-        print("The word is",secret,"!")
-
-else:
+j=0
+while j<5:
+    j+=1
+    guess=input()
+    code=[]
+    i=0
     for i in range(5):
           if guess[i]==secret[i]:
                 code.append("green")
@@ -16,8 +15,14 @@ else:
                 code.append("yellow")
           else:
                 code.append("grey")
+    print(code)  
+    if guess==secret:
+        print("The word is",secret,"!")
+        break
+if j==5:
+     print("You have used all your guesses. The word was: ", secret)
 
-print(code)           
+        
         
     
    
