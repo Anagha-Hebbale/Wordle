21bbcd00dc468aa449ae1f1dff849c807df79ed6
Anagha-Hebbale <hebbaleanagha@gmail.com>
2026-07-19 13:20:12 +0530

    game

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 print("Welcome to Wordle!")
+
+secret="apple"
+print("Guess the 5-Letter Word:")
+guess=input()
+code=[]
+i=0
+if guess==secret:
+        print("The word is",secret,"!")
+
+else:
+    for i in range(5):
+          if guess[i]==secret[i]:
+                code.append("green")
+          elif guess[i] in secret:
+                code.append("yellow")
+          else:
+                code.append("grey")
+
+print(code)           
+        
+    
+   
